a09c702fb183280721aba1bcead3abce447a3a64
Jonny <jonnyar226@gmail.com>
2026-03-04 23:16:21 +0500

    refactor: multiple channels

--- index.js
+++ index.js
@@ -10,56 +10,58 @@ const client = new Client({
 client.on("clientReady", async () => {
 	console.log(`Logged in as ${client.user.tag}`);
 
-	const channel = await client.channels.fetch(config.channelId);
-	const msg = await channel.send("Fetching server statuses...");
+	for (const channelId of config.channelIds) {
+		const channel = await client.channels.fetch(channelId);
+		const msg = await channel.send("Fetching server statuses...");
 
-	const updateStatus = async () => {
-		const embed = new EmbedBuilder()
-			.setTitle("Server Status")
-			.setColor("#5ad65c")
-			.setTimestamp();
+		const updateStatus = async () => {
+			const embed = new EmbedBuilder()
+				.setTitle("Server Status")
+				.setColor("#5ad65c")
+				.setTimestamp();
 
-		for (const server of config.servers) {
-			const [ip, portStr] = server.split(":");
-			const port = Number(portStr);
+			for (const server of config.servers) {
+				const [ip, portStr] = server.split(":");
+				const port = Number(portStr);
 
-			try {
-				const state = await GameDig.query({
-					type: "garrysmod",
-					host: ip,
-					port: port,
-				});
+				try {
+					const state = await GameDig.query({
+						type: "garrysmod",
+						host: ip,
+						port: port,
+					});
 
-				let playerList = " - There is no one on the server...";
-				if (state.players.length > 0) {
-					playerList = state.players
-						.map(p => p.name || "*Connecting ...*")
-						.map(s => ` - ${s}`)
-						.join("\n");
+					let playerList = " - There is no one on the server...";
+					if (state.players.length > 0) {
+						playerList = state.players
+							.map(p => p.name || "*Connecting ...*")
+							.map(s => ` - ${s}`)
+							.join("\n");
+					}
+
+					embed.addFields([
+						{
+							name: `${state.name} (${ip}:${port})`,
+							value: `Map: \`${state.map}\`\nPlayers: \`${state.numplayers}/${state.maxplayers}\`\n${playerList}\n\`steam://connect/${ip}:${port}\``,
+						},
+					]);
+				} catch (err) {
+					console.log(err);
+					embed.addFields([{ name: `Server ${ip}:${port}`, value: "Server offline..." }]);
 				}
-
-				embed.addFields([
-					{
-						name: `${state.name} (${ip}:${port})`,
-						value: `Map: \`${state.map}\`\nPlayers: \`${state.numplayers}/${state.maxplayers}\`\n${playerList}\n\`steam://connect/${ip}:${port}\``,
-					},
-				]);
-			} catch (err) {
-				console.log(err);
-				embed.addFields([{ name: `Server ${ip}:${port}`, value: "Server offline..." }]);
 			}
-		}
 
-		embed.addFields({
-			name: "Last check",
-			value: `<t:${Math.round(Date.now() / 1000)}:R>`,
-		});
+			embed.addFields({
+				name: "Last check",
+				value: `<t:${Math.round(Date.now() / 1000)}:R>`,
+			});
 
-		await msg.edit({ content: null, embeds: [embed] });
-	};
+			await msg.edit({ content: null, embeds: [embed] });
+		};
 
-	updateStatus();
-	setInterval(updateStatus, 60000);
+		updateStatus();
+		setInterval(updateStatus, 60000);
+	}
 });
 
 client.login(config.token);
